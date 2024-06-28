85d6169f2a424a5712b2f918452e04453a3b4172
hishmatrai <hishmatrai.2018@gamil.com>
2024-06-28 07:36:43 -0700

    update

--- src/pages/classs-component.js
+++ src/pages/classs-component.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Layout } from "../conmponents";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 class ClassComponent extends React.Component {
   constructor() {
     super();
@@ -9,9 +10,20 @@ class ClassComponent extends React.Component {
     };
   }
 
-   updateBgColor = () =>{
-   this.setState({bgColor:"gray"})
+  componentDidMount() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user);
+      } else {
+        console.log("login false");
+      }
+    });
   }
+
+  updateBgColor = () => {
+    this.setState({ bgColor: "gray" });
+  };
   render() {
     return (
       <Layout title="Contact">
